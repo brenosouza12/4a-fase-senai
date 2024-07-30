3e91ccdf2d70e716bfcbc584b3116e08624b111e
brenosouza12 <brenosouzadev12@gmail.com>
2024-07-30 17:10:49 -0300

    20240730

---
+++ React-JS-2024/20240730_1stProject/src/components/Contents.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import './Contents.css'
+
+function Contents() {
+    
+  return (
+
+    <div className='contents-container'>
+
+      <p>
+
+        The Luna Twisted Show - A new series by Luna Twisted - Out May 20
+        
+        </p>
+
+    </div>
+
+  )
+
+}
+
+export default Contents
---
+++ React-JS-2024/20240730_1stProject/src/components/Navbar/index.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './index.css'
+
+function Navbar() {
+
+  return (
+
+    <div className='navbar-container'>
+
+       <h1>VR-12 Series</h1>
+
+    </div>
+
+  )
+
+}
+
+export default Navbar
+
