4e1077fe8e8fb4889b1d613462b2c15416d85589
brenosouza12 <brenosouzadev12@gmail.com>
2024-09-10 15:17:53 -0300

    20240910

---
+++ React-JS-2024/20240910_5thProject/src/components/FaxaLogin.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function FaxaLogin() {
+    return (
+        <div>
+           <p>Efetue o login</p>
+        </div>
+    )
+}
+
+export default FaxaLogin
---
+++ React-JS-2024/20240910_5thProject/src/components/UsuarioLogado.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function UsuarioLogado() {
+    return (
+        <div>
+
+            <p>Olá usuário</p>
+
+        </div>
+    )
+}
+
+export default UsuarioLogado
