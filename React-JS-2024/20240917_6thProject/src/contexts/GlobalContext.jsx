a3ea133fc7bbda7c05b7d071a1033aa400d3a009
brenosouza12 <brenosouzadev122gmail.com>
2024-09-17 16:41:22 -0300

    20240917

--- React-JS-2024/20240917_6thProject/src/contexts/GlobalContext.jsx
+++ React-JS-2024/20240917_6thProject/src/contexts/GlobalContext.jsx
@@ -1,13 +1,29 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 
 export const GlobalContext = createContext()
 
 export const GlobalContextProvider = ({ children }) => {
 
     let usuarioLogado = 'Clara Wayland'
+    const [usuarios, setUsuarios] = useState([
+
+        {
+            id: Date.now(),
+            nome: 'Elizabeth Lindemann'
+            , email: 'lizzielind@gmail.com.br'
+
+        },
+
+        {
+            id: Date.now()+ 1,
+            nome: 'Elizabeth Spectrum'
+            , email: 'lizziespk12@gmail.com.br'
+
+        }
+    ])
 
     return (
-        <GlobalContext.Provider value={{usuarioLogado}}>
+        <GlobalContext.Provider value={{ usuarioLogado, usuarios, setUsuarios }}>
             {children}
         </GlobalContext.Provider>
     )
--- React-JS-2024/20240917_6thProject/src/pages/Home.jsx
+++ React-JS-2024/20240917_6thProject/src/pages/Home.jsx
@@ -3,14 +3,26 @@ import Navbar from '../components/Navbar'
 import { GlobalContext } from '../contexts/GlobalContext'
 
 function Home() {
-    const { usuarioLogado } = useContext(GlobalContext)
+    const { usuarioLogado, usuarios, setUsuarios } = useContext(GlobalContext)
     return (
         <div>
             <Navbar />
             <h1>Home</h1>
-            {usuarioLogado}
+            Olá {usuarioLogado}
             <p>Hello World</p>
+            <div>
+                {
+                    usuarios.map((usuario) => (
+                        <div key={usuario.id}>
+                            <p>Nome: {usuario.nome}</p>
+                            <p>Email: {usuario.email}</p>
+                            <p>{usuario.id}</p>
+                        </div>
+                    ))
+                }
+            </div>
         </div>
+
     )
 }
 
